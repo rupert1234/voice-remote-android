6296a6fc8f86253936cefd95c7862864e67a6cc5
danijel.korzinek <danijel.korzinek@35e5ada6-0767-11df-8d27-21ff5afbef34>
2010-02-08 21:28:08 +0000

    Added automatic startup for Julius with error checks and clean shutdown. When closing the main program Julius process is automatically killed. Created a console window that contains a history of Julius STDOUT and STDERR outputs.
    Problems:
    -the console hangs when Julius is awaiting connection. This is inconsistent with the system console operation.
    -need to add options to the settings window
    -need to add process and connection monitor to the settings window (with restart option)
    -there has to be a delay between the process starting and attempting connection

--- VoiceRemoteServer/src/Main.java
+++ VoiceRemoteServer/src/Main.java
@@ -1,13 +1,32 @@
 public class Main {
 
+	public static JuliusStarter julius_starter;
+	public static ServerSettings settings;
+	
 	public static void main(String[] args) {
 
-		ServerSettings settings=new ServerSettings();
+		settings=new ServerSettings();
 		settings.setVisible(true);
 		
+		julius_starter=new JuliusStarter();
+		Thread js_th=new Thread(julius_starter);
+		js_th.start();		
+		
+		try{
+		Thread.sleep(1000);
+		}catch(Exception e){}
+		
 		JuliusClient client=new JuliusClient("localhost",5530,settings.getActions());
-		Thread th = new Thread(client);
-		th.start();
+		Thread cl_th = new Thread(client);
+		cl_th.start();
+		
+		Runtime.getRuntime().addShutdownHook(new Thread()
+		{
+			public void run()
+			{
+				julius_starter.terminate();
+			}
+		});
 		
 	}
 
