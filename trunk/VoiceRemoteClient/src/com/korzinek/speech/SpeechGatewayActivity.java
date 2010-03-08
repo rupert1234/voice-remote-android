1f51f9dbbd70e648c0a573ae41a4ee647e8d9c7e
danijel.korzinek <danijel.korzinek@35e5ada6-0767-11df-8d27-21ff5afbef34>
2010-03-08 23:36:38 +0000

    Massive code cleanup. This is about all that needs to be done for version 1.0 of the client.
    Some items:
    -changed icons
    -changed layouts
    -added error dialog instead of scrollable textview
    -fixed settings (saves whenever focus changes away from settings)
    -fixed sending data: now there are 2 threads, one reads audio and adds buffers to the other thread; the other thread sends data until all buffers are gone

--- trunk/VoiceRemoteClient/src/com/korzinek/speech/SpeechGatewayActivity.java
+++ trunk/VoiceRemoteClient/src/com/korzinek/speech/SpeechGatewayActivity.java
@@ -1,6 +1,7 @@
 package com.korzinek.speech;
 
 import android.app.Activity;
+import android.app.Dialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.media.AudioFormat;
@@ -18,6 +19,7 @@ import android.view.View.OnTouchListener;
 import android.widget.Button;
 import android.widget.TextView;
 
+
 public class SpeechGatewayActivity extends Activity implements OnTouchListener, OnClickListener{
 	
 
@@ -28,13 +30,15 @@ public class SpeechGatewayActivity extends Activity implements OnTouchListener, 
 		super.onDestroy();
 	}
 
-	TextView console;
 	AudioRecord audioInput;
 	AudioReader audioReader;
 	Thread audioReaderThread;
 	Handler consoleHandler;
 	String server;
 	int port;
+	String dialogMessage;
+	
+	Dialog errorDialog;
 	
     /** Called when the activity is first created. */
     @Override
@@ -45,16 +49,12 @@ public class SpeechGatewayActivity extends Activity implements OnTouchListener, 
         Button button = (Button)findViewById(R.id.record_button);
         button.setOnTouchListener(this);
         
-        Button exit = (Button)findViewById(R.id.Exit);
-        exit.setOnClickListener(this);        
-		
-		console=(TextView)findViewById(R.id.console);
+        
 		consoleHandler = new Handler() {
-
 			@Override
 			public void handleMessage(Message msg) {
-				String text=msg.getData().getString("msg");
-				console.append(text);
+				dialogMessage=msg.getData().getString("msg");
+				showDialog(0);
 			}
 			
 		};
@@ -75,22 +75,59 @@ public class SpeechGatewayActivity extends Activity implements OnTouchListener, 
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		menu.add("Setings");
+		menu.add(0,1,0,"Setings");
+		menu.add(0,0,0,"Exit");
 		return true;
+	}
+
+	@Override
+	protected Dialog onCreateDialog(int id) {
+		
+		if(id==0)
+		{
+	        errorDialog=new Dialog(this);
+	        errorDialog.setContentView(R.layout.error_dialog);
+	        errorDialog.setTitle("Error");
+	        Button button = (Button)errorDialog.findViewById(R.id.Button01);
+	        button.setOnClickListener(this);
+			return errorDialog;
+		}
+		
+		return null;
+	}
+
+	@Override
+	protected void onPrepareDialog(int id, Dialog dialog) {
+		if(id==0)
+		{
+			TextView text=(TextView)dialog.findViewById(R.id.TextView01);
+			if(dialog.isShowing())
+				text.append("\n"+dialogMessage);
+			else
+				text.setText(dialogMessage);
+			
+		}
 	}
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		
-		Intent intent=new Intent();
-		intent.setClass(SpeechGatewayActivity.this, Settings.class);
-		startActivity(intent);
+		if(item.getItemId()==0)
+		{
+			finish();
+		}
+		else
+		{
+			Intent intent=new Intent();
+			intent.setClass(SpeechGatewayActivity.this, SettingsActivity.class);
+			startActivity(intent);
+		}
 			
 		return true;
 	}
 
 	public void onClick(View v) {			
-		finish();				
+		errorDialog.cancel();			
 	}
 
 
