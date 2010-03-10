d45d8f2f2353652077e383baa3b2efb0a0c2c268
danijel.korzinek <danijel.korzinek@35e5ada6-0767-11df-8d27-21ff5afbef34>
2010-03-10 12:48:15 +0000

    Replaced transcription cache from XML to CSV cause Java crashed on large XML files.

--- trunk/VoiceRemoteServer/src/TranscriptionCache.java
+++ trunk/VoiceRemoteServer/src/TranscriptionCache.java
@@ -1,23 +1,14 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.util.Iterator;
 import java.util.Map;
 
 import javax.swing.JOptionPane;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.transform.OutputKeys;
-import javax.xml.transform.Result;
-import javax.xml.transform.Source;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
 
 import org.apache.commons.collections15.map.HashedMap;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.NodeList;
-import org.w3c.dom.Text;
 
 
 public class TranscriptionCache {
@@ -49,34 +40,17 @@ public class TranscriptionCache {
 	public void save()
 	{
 		try{
-			DocumentBuilderFactory dbfac = DocumentBuilderFactory.newInstance();
-			DocumentBuilder docBuilder = dbfac.newDocumentBuilder();
-			Document doc = docBuilder.newDocument();
-				
-			Element root=doc.createElement("Transcriptions");
-			doc.appendChild(root);
+			BufferedWriter writer=new BufferedWriter(new FileWriter(new File("transcription_cache.csv")));
+			
 						
 			Iterator<Map.Entry<String, String>> it=cache.entrySet().iterator();
 			while(it.hasNext())
 			{
-				Map.Entry<String, String> pair=it.next();
-				
-				Element el=doc.createElement("Word");
-				el.setAttribute("Text", pair.getKey());
-
-				Text args=doc.createTextNode(pair.getValue());
-				el.appendChild(args);
-				
-				root.appendChild(el);
+				Map.Entry<String, String> pair=it.next();				
+				writer.write(pair.getKey()+" "+pair.getValue()+"\n");				
 			}
 			
-			TransformerFactory transfac = TransformerFactory.newInstance();
-            Transformer trans = transfac.newTransformer();
-            trans.setOutputProperty(OutputKeys.INDENT, "yes");
-            
-            Source source = new DOMSource(doc);
-            Result result = new StreamResult(new File("transcription_cache.xml"));
-            trans.transform(source, result);
+			writer.close();
 			
 		} catch (Exception e) {
 			JOptionPane.showMessageDialog(null,""+e);
@@ -85,34 +59,26 @@ public class TranscriptionCache {
 	
 	public void load()
 	{
-		File file;
-		DocumentBuilderFactory dbf;
-		DocumentBuilder db;
-		Document document;
-		try {
-			file = new File("transcription_cache.xml");
-			dbf = DocumentBuilderFactory.newInstance();
-			db = dbf.newDocumentBuilder();
-			document = db.parse(file);
-		} catch(Exception e) {return;}
 		
-		try {		
+		try{
+			BufferedReader reader=new BufferedReader(new FileReader(new File("transcription_cache.csv")));
 			
-			document.getDocumentElement().normalize();
+			cache.clear();
 			
-			if(document.getDocumentElement().getNodeName()!="Transcriptions")
-				throw(new Exception("Root is not Transriptions!"));
+			String line,word,trans;
+			int pos;
+			while((line=reader.readLine())!=null)
+			{
+				pos=line.indexOf(' ');
+				word=line.substring(0,pos);
+				trans=line.substring(pos+1);
+				setTranscription(word, trans);
+			}
 			
-			NodeList item_list = document.getElementsByTagName("Word");
-
-			for (int i = 0; i < item_list.getLength(); i++) {
-				Element word = (Element)item_list.item(i);		
-				String text=word.getAttribute("Text");
-				String trans=word.getChildNodes().item(0).getNodeValue();
-				cache.put(text,trans);
-			}			
-		} catch (Exception e) {
-			JOptionPane.showMessageDialog(null,"grammar_settings.xml parsing error:\n"+e);
+			reader.close();
+			
+		}catch(Exception e) {
+			JOptionPane.showMessageDialog(null,""+e);
 		}
 	}
 }
